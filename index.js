fc2e2997d4efa23fb8e90b79e430d5e068b791bc
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-08-02 12:40:21 -0400

    package requirements

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-// TODO: Include packages needed for this application
+
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -9,5 +14,6 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 function init() {}
 
-// Function call to initialize app
+
+// INITIALIZE
 init();
