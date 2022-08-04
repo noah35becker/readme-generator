ea92c81818720dc8e6f0b2b8a60c915e8d3a5215
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-08-04 00:27:24 -0400

    update Contributor Covenant link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,7 +10,7 @@ const pullLicenseBadge = (licenseName) => {
 
 // Get markdown for Contributor Covenant badge image
 const pullContribCovenantBadge = () => 
-    '[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](https://www.contributor-covenant.org/)';
+    '[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](https://www.contributor-covenant.org/version/2/1/code_of_conduct/)';
 
 
 // Get link for a given license
